8fad36ce020fa67139a700818141798ceb0fb86b
Elina <Elina.Hlushchenko@cs.khpi.edu.ua>
2021-02-07 15:25:23 +0200

    +Lab5(b)

---
+++ Labs/Lab5/Lab5(b)/Lab5(b).c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	unsigned int k;
+	printf("Количество грибов: ");
+	scanf_s("%d", &k);
+	printf("У меня %u гриб", k);
+
+	if ((k % 10 == 1) && (k % 100 != 11)) {
+		printf("\n");
+	}
+	else if ((k % 10 == 2) && (k % 100 != 12)) {
+		printf("а\n");
+	}
+	else if ((k % 10 == 3) && (k % 100 != 13)) {
+		printf("а\n");
+	}
+	else if ((k % 10 == 4) && (k % 100 != 14)) {
+		printf("а\n");
+	}
+	else {
+		printf("ов\n");
+	}
+
+	return 0;
+}
