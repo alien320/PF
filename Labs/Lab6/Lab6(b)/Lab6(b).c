d43dba9e13ac609fba51af138fbf454b7ba0379b
Elina <Elina.Hlushchenko@cs.khpi.edu.ua>
2021-02-11 19:42:14 +0200

    +7

---
+++ Labs/Lab6/Lab6(b)/Lab6(b).c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+	printf("Решите следующие примеры:\n");
+
+    int a, b, c, n, check, points = 0;
+	for (n = 0; n < 10; n++)
+	{
+		a = rand() % 10;
+		b = rand() % 10;
+		printf("%d * %d = ", a, b);
+		scanf_s("%d", &c);
+
+		check = a * b;
+		if (check == c)
+		{
+			points += 1;
+		}
+	}
+
+	printf("Результат: %d/%d\n", points, n);
+
+	return 0;
+}
---
+++ Labs/Lab7/Lab7.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x, y, n, dh;
+    int h;
+
+    printf(" --------------------\n");
+    printf("|   x   |      y     |\n");
+    printf(" --------------------\n");
+
+    for (n = 0; n < 3; n++)
+    {
+        for (x = 0; x < 8; x += 0.25)
+        {
+            if (x < 2) y = sqrt(1 - (x - 1) * (x - 1));
+
+            else if (x < 4) y = sqrt(1 - (x - 3) * (x - 3));
+
+            else if (x < 6) y = - sqrt(1 - (x - 5) * (x - 5));
+
+            else  y = - sqrt(1 - (x - 7) * (x - 7));
+
+            if (x + n * 8 == 20) break;
+            printf("| %5.2lf | %10.7lf | ", x + n * 8, y);
+
+            dh = (y + 1) * 10;
+            h = dh;
+            if (dh - h > 0.5) h++;
+
+            for (; h > 0; h--) printf(" ");
+            printf("*\n");
+        }
+    }
+
+    printf(" --------------------\n");
+    return 0;
+}
