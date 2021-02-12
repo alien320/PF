9cfb7fa0432659e2e6a3bac0b52db6e0a445a096
Elina <Elina.Hlushchenko@cs.khpi.edu.ua>
2021-02-12 11:36:42 +0200

    +lab9

---
+++ Labs/Lab9/Lab9.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#define S 9
+
+int A[S][S];
+
+int main()
+{
+	int k, n = 1, j, r, l;
+
+	for (k = 1; k <= 4; k++)
+	{
+		for (j = k - 1; j < S - k + 1; j++) A[k - 1][j] = n++;
+
+		for (j = k; j < S - k + 1; j++) A[j][S - k] = n++;
+
+		for (j = S - k - 1; j >= k - 1; --j) A[S - k][j] = n++;
+
+		for (j = S - k - 1; j >= k; j--) A[j][k - 1] = n++;
+	}
+
+	A[4][4] = S * S;
+
+	for (l = 0; l < S; l++)
+	{
+		for (r = 0; r < S; r++)
+		{
+			printf("%3d", A[l][r]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
