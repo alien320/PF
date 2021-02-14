daca6fc2216c4674c54c76b549958ea2df6d8b82
Elina <Elina.Hlushchenko@cs.khpi.edu.ua>
2021-02-14 19:55:45 +0200

    +lab10,11

---
+++ Labs/Lab10/Lab10/Lab10.c
@@ -0,0 +1,79 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+
+#define ARRSIZE 16
+#define ARRSIZE2 4
+
+void PrintArray(int arr[])
+{
+    for (int i = 0; i < ARRSIZE; i++)
+    {
+        printf(" %2d ", arr[i]);
+    }
+    printf("\n\n");
+}
+
+void PrintArray2(int arr2[][ARRSIZE2])
+{
+    for (int i = 0; i < ARRSIZE2; i++)
+    {
+        for (int j = 0; j < ARRSIZE2; j++)
+        {
+            printf(" %2d ", arr2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void TransformArray(int arr[])
+{
+    int arr2[ARRSIZE2][ARRSIZE2];
+
+    for (int i = 0; i < ARRSIZE2; i++)
+    {
+        for (int j = 0; j < ARRSIZE2; j++)
+        {
+            arr2[i][j] = arr[i * 4 + j];
+        }
+    }
+
+    PrintArray2(arr2);
+
+    int a = 0, d;
+    for (int i = 0; i < ARRSIZE2; i++)
+    {
+        d = arr2[i][a];
+        for (int j = 0; j < ARRSIZE2; j++)
+        {
+            arr2[i][j] *= d;
+        }
+        a++;
+    }
+
+    PrintArray2(arr2);
+
+    for (int i = 0; i < ARRSIZE2; i++)
+    {
+        for (int j = 0; j < ARRSIZE2; j++)
+        {
+            arr[i * 4 + j] = arr2[i][j];
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+
+    int arr[ARRSIZE];
+    for (int i = 0; i < ARRSIZE; i++) arr[i] = rand() % 10;
+    
+    PrintArray(arr);
+    TransformArray(arr);
+    PrintArray(arr);
+
+    return 0;
+}
---
+++ Labs/Lab11/Lab11.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int num()
+{
+	int n;
+	scanf_s("%d", &n);
+
+	if (n > 0)
+	{
+		num();
+		printf("%d ", n);
+	}
+	else
+	{
+		printf("0 ");
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	printf("Введите последовательность чисел: ");
+	num();
+	printf("\n");
+}
